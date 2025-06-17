fb1713a43b8db508509bc1ac871a89b81ab02a84
alii16 <boy932535@gmail.com>
2025-06-17 13:17:41 +0800

    v-5.0.3

--- js/home.js
+++ js/home.js
@@ -34,12 +34,16 @@ function parseTarget(targetStr) {
   let suffix = "";
   let numberPart = targetStr;
 
-  if (targetStr.endsWith("k")) {
-    suffix = "K";
+  if (targetStr.endsWith("0")) {
+    suffix = "+";
     numberPart = targetStr.slice(0, -1);
-    return { number: Number(numberPart) * 10, suffix };
+    return { number: Number(numberPart) *10, suffix };
   } else if (targetStr.endsWith("%")) {
     suffix = "%";
+    numberPart = targetStr.slice(0, -1);
+    return { number: Number(numberPart), suffix };
+  } else if (targetStr.endsWith("+")) {
+    suffix = "K+";
     numberPart = targetStr.slice(0, -1);
     return { number: Number(numberPart), suffix };
   } else {
@@ -50,8 +54,8 @@ function parseTarget(targetStr) {
 function animateCounter(element, targetStr) {
   const { number: target, suffix } = parseTarget(targetStr);
   let current = 0;
-  const duration = 2000; // durasi animasi dalam milidetik
-  const frameRate = 30; // frame per detik
+  const duration = 2000;
+  const frameRate = 30;
   const totalFrames = Math.round(duration / (1000 / frameRate));
   const increment = target / totalFrames;
   const timer = setInterval(() => {
@@ -75,7 +79,7 @@ const observer = new IntersectionObserver((entries) => {
     if (entry.isIntersecting && entry.target.classList.contains("counter")) {
       const targetStr = entry.target.getAttribute("data-target");
       animateCounter(entry.target, targetStr);
-      observer.unobserve(entry.target); // animasi hanya dilakukan sekali
+      observer.unobserve(entry.target);
       entry.target.classList.add("fade-in-up");
     }
   });
